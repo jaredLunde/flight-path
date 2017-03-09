e6851f480159368f1bf82a5c890cde9e2ff4e0fb
Jared Lunde <jared.lunde@gmail.com>
2017-03-09 22:49:01 +0900

    Fixed .babelrc w/ correct plugins/presets

--- src/index.dist.js
+++ src/index.dist.js
@@ -6,18 +6,5 @@
 //   http://github.com/jaredlunde/flight-path                                 //
 //                                                                            //
 ////////////////////////////////////////////////////////////////////////////////
-export del from './del'
-export EncodedFormRequest from './EncodedFormRequest'
-export get from './get'
-export head from './head'
-export JsonRequest from './JsonRequest'
-export JsonResponse from './JsonResponse'
-export options from './options'
-export patch from './patch'
-export post from './post'
-export put from './put'
-export Request from './Request'
-export Response from './Response'
-export ship from './ship'
-export TextResponse from './TextResponse'
-export Xhr from './Xhr'
+import 'babel-polyfill'
+export * from './index'
